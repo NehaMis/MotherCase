b8c6e4414dd70432271efdbea816c3a493057eaf
zdanish <zeeshan.danish@vendekin.com>
2020-10-08 22:24:54 +0800

    update

--- components/SearchFood.js
+++ components/SearchFood.js
@@ -1,35 +1,181 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity, FlatList, Alert} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity, FlatList, Alert, TextInput, DevSettings } from 'react-native';
 import Logo from './Logo';
 import EmailAndPassword from './EmailAndPassword';
 import LinearGradient from 'react-native-linear-gradient';
-import {px} from "./util/dimensions";
-import { TextInput } from 'react-native-gesture-handler';
+import { px } from "./util/dimensions";
 
 export default class SearchFood extends Component {
 
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.objRecipe = {};
-   
-     
+    this.objRecipe = {};
+    this.state = {
+      isSearching: false,
+      arrSearchedProducts: []
+    }
+    this.arrSelectedProducts = {};
+    this.arrProducts = [
+      {
+        name: "Apple",
+        image_url: require("../assets/apple.png"),
+        description: "Raw Apple",
+        qty_type: "qty",
+        calories: "100",
+        qty: 1,
+      },
+      {
+        name: "Oats",
+        image_url: require("../assets/oats.jpg"),
+        description: "Multigrain Oats with lots of fibres",
+        qty_type: "gm",
+        calories: "200",
+        qty: 200,
+      },
+      {
+        name: "Water",
+        image_url: require("../assets/water.jpg"),
+        description: "Mineral Water",
+        qty_type: "Glass",
+        calories: "0",
+        qty: 1,
+      },
+      {
+        name: "Poha",
+        image_url: require("../assets/poha.jpg"),
+        description: "Poha with peanuts",
+        qty_type: "gm",
+        calories: "250",
+        qty: 200,
+      },
+
+    ]
+
+  }
+
+  loadRecipe(name) {
+    var arrProducts = this.arrProducts.filter(data => {
+      return data.name.toLowerCase().startsWith(name.toLowerCase());
+    });
+
+
+    this.setState({ arrSearchedProducts: arrProducts || [] });
+
+  }
+
+  handleSearch(text) {
+    if (text === '' || text.length === 0) {
+      this.handleStopSearch();
+    } else {
+      // if (this.state.boolSearchWithNum) {
+      //   if (text.length > 9) {
+      //     this.loadRecipe(text);
+      //   } else {
+      //     this.setState({userData: '', arrUserRecharge: []});
+      //   }
+      //   this.setState({mobile_no: text});
+      // } else {
+
+      if (this.typingTimeout) {
+        clearTimeout(this.typingTimeout);
+      }
+
+      this.typingTimeout = setTimeout(() => {
+        this.loadRecipe(text);
+      }, 100);
+
+      this.setState({ recipe_name: text, isSearching: true });
+
+      //}
+    }
+  }
+
+  handleStopSearch() {
+    this.setState({ recipe_name: "", isSearching: false, arrSearchedProducts: [] });
+  }
+
+  addProducts(item) {
+    if (!this.arrSelectedProducts[item.name]) {
+      this.arrSelectedProducts[item.name] = {
+        qty: 0,
+        name: item.name,
+
+      }
+    }
+
+    this.arrSelectedProducts[item.name].qty += item.qty;
+    this.setState({ render: true })
+  }
+
+  removeProduct(item) {
+    if (this.arrSelectedProducts[item.name] && this.arrSelectedProducts[item.name].qty > 0) {
+      this.arrSelectedProducts[item.name].qty -= item.qty;
+    }
+
+    this.setState({render:true});
+  }
+
+  renderFlatList({ item }) {
+    return (
+      <View style={styles.card}>
+        <View style={{ width: "30%" }}>
+          <Image source={item.image_url} style={{ width: px(70), height: px(70) }} />
+        </View>
+        <View style={{ width: "50%" }}>
+          <Text style={{ fontSize: px(14) }}>{item.name}</Text>
+          <Text style={{ fontSize: px(10), marginTop: px(5) }}>Quantity: {item.qty} {item.qty_type}</Text>
+          <Text style={{ fontSize: px(10) }}>Calories: {item.calories}</Text>
+        </View>
+        <View style={{ width: "20%", alignItems: "center" }}>
+          <TouchableOpacity style={styles.buttons} ><Text style={{ fontSize: px(16), color: "white", fontWeight: "bold" }} onPress={()=>this.removeProduct(item)}>-</Text></TouchableOpacity>
+          <View style={{ fontSize: px(10), height: px(20), justifyContent: "center" }}>
+            {this.arrSelectedProducts[item.name] && this.arrSelectedProducts[item.name].qty > 0 ?
+              <Text style={{ fontSize: px(10) }}>{this.arrSelectedProducts[item.name].qty} {item.qty_type}</Text>
+              :
+              null}
+          </View>
+          <TouchableOpacity style={styles.buttons} ><Text style={{ fontSize: px(14), color: "white", fontWeight: "bold" }} onPress={() => this.addProducts(item)}>+</Text></TouchableOpacity>
+        </View>
+      </View>
+    )
   }
 
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.card}>
-              <TextInput></TextInput>
-              <TouchableOpacity  style={styles.searchButton}> <Image source = {require('../assets/greySearch.png')}></Image>
-          </TouchableOpacity>
-          </View>  
-          <View>
-             <Text>Start Typing the food name </Text>
-             <Text>Search by typing the name of the food you had for breakfast </Text>
-             </View> 
-            <Image source = {require('../assets/greySearch.png')}></Image>
-          
+          {/* <TouchableOpacity style={styles.searchButton}> */}
+
+          {/* </TouchableOpacity> */}
+
+          <Image source={require('../assets/greySearch.png')} />
+          <TextInput value={this.state.recipe_name} onChangeText={(text) => this.handleSearch(text)} style={{ paddingLeft: px(10), width: "100%" }} placeholder="Start Type..."></TextInput>
+
+        </View>
+
+        {!this.state.isSearching ?
+          <View style={{ borderRadius: 10, padding: px(10), flexDirection: 'row', marginTop: px(20), borderColor: "grey", borderWidth: 1, marginLeft: "5%", marginRight: "5%", width: "90%", backgroundColor: "white" }}>
+            <Image style={{ marginTop: px(15) }} source={require('../assets/greySearch.png')} />
+            <View style={{ width: "100%", paddingLeft: px(15) }}>
+              <Text style={{ fontWeight: "bold" }}>Start Typing the food name </Text>
+              <Text style={{ marginTop: px(5), width: "80%" }}>Search by typing the name of the food you had for breakfast </Text>
+            </View>
+          </View>
+          : null}
+
+        <FlatList
+          vertical
+          data={this.state.arrSearchedProducts}
+          renderItem={(e) => this.renderFlatList(e)}
+          // ItemSeparatorComponent={() => this.flatlistSeperator()}
+          keyExtractor={(item, index) => {
+            return index;
+          }}
+          style={{ width: "100%" }}
+          scrollEnabled={true}
+        />
+
       </View>
     );
   }
@@ -41,9 +187,17 @@ const styles = StyleSheet.create({
     //s justifyContent: 'center',
     alignItems: 'center',
   },
-  card:{
-    justifyContent: 'center',
-     alignItems: 'center',
+  buttons: {
+    height: px(20),
+    width: px(20),
+    backgroundColor: "#3333cc",
+    borderRadius: 5,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  card: {
+    flexDirection: "row",
+    alignItems: 'center',
     alignSelf: "center",
     backgroundColor: 'white',
     minHeight: px(30),
@@ -61,10 +215,10 @@ const styles = StyleSheet.create({
     elevation: 3,
     borderRadius: 5,
     overflow: "hidden",
-
-    },
-    searchButton:{
-        height: 50,
-        width: 100,
-    }
+    paddingLeft: px(5)
+  },
+  searchButton: {
+    height: 50,
+    width: 100,
+  }
 });
--- components/TrackDiet.js
+++ components/TrackDiet.js
@@ -1,89 +1,99 @@
-import React, { Component } from 'react' ;
-import { View, Text, StyleSheet, FlatList, Alert} from 'react-native';import { TouchableOpacity } from 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, FlatList, Alert } from 'react-native'; import { TouchableOpacity } from 'react-native-gesture-handler';
 ;
-import {px} from "./util/dimensions";
+import { px } from "./util/dimensions";
 
 export default class TrackDiet extends Component {
-    doc = [
-        {name: 'Breakfast'},
-        {name: 'Morning Snack'},
-        {name: 'Lunch'},
-        {name: 'Evening Snack'},
-        {name: 'Dinner'},
-    ];
+  doc = [
+    { name: 'Breakfast' },
+    { name: 'Morning Snack' },
+    { name: 'Lunch' },
+    { name: 'Evening Snack' },
+    { name: 'Dinner' },
+  ];
 
-    constructor(props) {
-        super(props);
-      }
-   
-
-    
-      showRecipe(recipe) {
-        this.props.navigation.navigate('SearchFood', {name: 'test'});
-      }
-    
-      renderFlatList({item}) {
-        return (
-          <View style={styles.card}>
-              <TouchableOpacity
-        style={{padding: px(10)}}
-        onPress={() => {
-          this.showRecipe(item);
-        }}>
-              <Text style={[styles.headerText, {color: "#239B56"}]}>{item.name}</Text>
-              </TouchableOpacity>
-              
-          </View>
-        );
-      }
-
-  
-      flatlistSeperator() {
-        return (
-          <View style={{height: 1, width: '100%',alignItems: "center"}}>
-            <View style={{height: 1, width: '90%', backgroundColor: '#ccc'}}>
-    
-            </View>
-          </View>
-        );
-      }
-
-      render() {
-        return (
-          <View style={styles.container}>
-                <Text style={styles.headerText}>What would you like to track ?</Text>
-            <FlatList
-              vertical
-              data={this.doc}
-              renderItem={(e) => this.renderFlatList(e)}
-              ItemSeparatorComponent={() => this.flatlistSeperator()}
-              keyExtractor={(item, index) => {
-                return index;
-              }}
-              style={{width: "100%"}}
-              scrollEnabled={true}
-            />
-          </View>
-        );
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalCalConsumed: 100
     }
-    
+  }
 
-const styles = StyleSheet. create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-     // backgroundColor:"#2c3e50",
-    },
-    headerText:{
-        fontSize: px(16),
-        margin: 5,
-    },
 
-    card:{
+
+  showRecipe(recipe) {
+    this.props.navigation.navigate('SearchFood', { name: 'test' });
+  }
+
+  renderFlatList({ item }) {
+    return (
+      <View style={styles.card}>
+        <TouchableOpacity
+          style={{ padding: px(10), flexDirection: "row", alignItems: "center" }}
+          onPress={() => {
+            this.showRecipe(item);
+          }}>
+            <View style={{flexDirection: "column", width: "90%"}}>
+            <Text style={[styles.headerText, { color: "#239B56" }]}>{item.name}</Text>
+            <Text>Calories Took: 0/200</Text>
+            </View>
+         
+          <View style={{width: px(20), height: px(20), borderRadius: px(10), backgroundColor: "blue", justifyContent: "center", alignItems: "center" }}>
+            <Text style={{color: "white", fontWeight: "bold"}}>+</Text>
+          </View>
+        </TouchableOpacity>
+
+      </View>
+    );
+  }
+
+
+  flatlistSeperator() {
+    return (
+      <View style={{ height: 1, width: '100%', alignItems: "center" }}>
+        <View style={{ height: 1, width: '90%', backgroundColor: '#ccc' }}>
+
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{ marginTop: px(10) }}>Today Calories Took: {this.state.totalCalConsumed} / 1000</Text>
+
+        <Text style={{ ...styles.headerText, ...{ marginTop: px(10) } }}>What would you like to track ?</Text>
+        <FlatList
+          vertical
+          data={this.doc}
+          renderItem={(e) => this.renderFlatList(e)}
+          ItemSeparatorComponent={() => this.flatlistSeperator()}
+          keyExtractor={(item, index) => {
+            return index;
+          }}
+          style={{ width: "100%" }}
+          scrollEnabled={true}
+        />
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
     justifyContent: 'center',
-     alignItems: 'center',
+    alignItems: 'center',
+    // backgroundColor:"#2c3e50",
+  },
+  headerText: {
+    fontSize: px(16),
+  },
+
+  card: {
+    justifyContent: 'center',
     alignSelf: "center",
     backgroundColor: 'white',
     minHeight: px(30),
@@ -102,5 +112,5 @@ const styles = StyleSheet. create({
     borderRadius: 5,
     overflow: "hidden",
 
-    }
-  });
+  }
+});
